da4a7cf71c76d7078b693ef939272e5c5da8b75f
ThePotatoArchivist <josiahfu@gmail.com>
2025-12-01 03:16:24 -0800

    Add config warnings

--- src/main/java/archives/tater/stagger/StaggerConfig.java
+++ src/main/java/archives/tater/stagger/StaggerConfig.java
@@ -1,18 +1,22 @@
 package archives.tater.stagger;
 
 import folk.sisby.kaleido.api.WrappedConfig;
-import folk.sisby.kaleido.lib.quiltconfig.api.annotations.DisplayNameConvention;
-import folk.sisby.kaleido.lib.quiltconfig.api.annotations.FloatRange;
-import folk.sisby.kaleido.lib.quiltconfig.api.annotations.SerializedNameConvention;
+import folk.sisby.kaleido.lib.quiltconfig.api.annotations.*;
+import folk.sisby.kaleido.lib.quiltconfig.api.metadata.ChangeWarning.Type;
 import folk.sisby.kaleido.lib.quiltconfig.api.metadata.NamingSchemes;
 
 @DisplayNameConvention(NamingSchemes.TITLE_CASE)
 @SerializedNameConvention(NamingSchemes.SNAKE_CASE)
 public class StaggerConfig extends WrappedConfig {
-    @FloatRange(min = 0, max = 100)
-    public double baseMaxPoise = 20;
+    @ChangeWarning(Type.RequiresRestart)
+    @IntegerRange(min = 0, max = 100)
+    public int baseMaxPoise = 20;
+
+    @ChangeWarning(Type.RequiresRestart)
     @FloatRange(min = 0, max = 2)
     public double basePoiseRate = 0.025;
+    
+    @ChangeWarning(Type.RequiresRestart)
     @FloatRange(min = 0, max = 10)
     public double baseStaggerLength = 5;
 }
